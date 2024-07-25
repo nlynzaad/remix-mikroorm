188f9dda397d5e3d2906a58a0a82388f03f45442
Nico Lynzaad <nlynzaad@zylangroup.com>
2024-07-25 17:06:55 +0200

    enhance styling a little bit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -40,32 +40,40 @@ export default function Index() {
 
 	return (
 		<div className="font-sans p-4">
-			<div className={'flex flex-row gap-4'}>
-				<div className={'flex flex-col'}>
+			<div className={'flex flex-row gap-8'}>
+				<div className={'flex flex-col border-2 border-black p-2'}>
+					<div><strong>Create User</strong></div>
+					<hr className={'h-[1px] border-none text-black bg-black mb-4 p-0'}/>
 					<Form method="POST">
-						<div className={'flex flex-row gap-1'}>
-							<label htmlFor="name">Name</label>
-							<input id='name' type="text" name='name' placeholder={'name'}
-								className={'border-2 border-gray-500'}/>
+						<div className={'flex flex-col'}>
+							<div className={'flex flex-row gap-5'}>
+								<div className={'flex flex-col gap-1'}>
+									<label htmlFor="name" className='pt-[2px]'>Name</label>
+									<label htmlFor="email" className='pt-[2px]'>Email</label>
+									<label htmlFor="userRole" className={'pt-[2px]'}>Role</label>
+								</div>
+								<div className={'flex flex-col gap-1'}>
+									<input id='name' type="text" name='name' placeholder={'name'}
+										className={'border-2 border-gray-500'}/>
+									<input id='email' type="text" name='email' placeholder={'email'}
+										className={'border-2 border-gray-500'}/>
+									<select id='userRole' name='userRole'
+										className={'border-2 border-gray-500 bg-white'}>
+										{userRoles.map((role) => <option key={role.id}
+											value={role.id}>{role.description}</option>)}
+									</select>
+								</div>
+							</div>
+							<button type={"submit"} className={'bg-blue-600 rounded p-1 mt-4'}>
+								Create
+							</button>
 						</div>
-						<div className={'flex flex-row gap-1 mb-2'}>
-							<label htmlFor="name">Email</label>
-							<input id='email' type="text" name='email' placeholder={'email'}
-								className={'border-2 border-gray-500'}/>
-						</div>
-						<div className={'flex flex-row gap-1 mb-2'}>
-							<label htmlFor="name">Role</label>
-							<select id='userRole' name='userRole'>
-								{userRoles.map((role) => <option key={role.id}
-									value={role.id}>{role.description}</option>)}
-							</select>
-						</div>
-						<button type={"submit"} className={'bg-blue-600 rounded p-1'}>
-							Submit
-						</button>
 					</Form>
 				</div>
 				<div className={'flex flex-col'}>
+					<div><strong>Users loaded</strong></div>
+					<hr className={'h-[1px] border-none text-black bg-black mb-2 p-0'}/>
+					{users.length === 0 ? <div>None</div> : null}
 					{users.map((user) => (
 						<Form method="POST" action={`/delete/${user.id}`} key={user.id}
 							onSubmit={(event) => {
@@ -76,23 +84,25 @@ export default function Index() {
 									event.preventDefault();
 								}
 							}}>
-							<div>
-								{user.id}: {user.name} - {user.email} ({user.userRole.description})
-								<button type={'submit'} className={'pl-2'}>X</button>
+							<div className={'justify-between flex flex-row'}>
+								<div>{user.id}: {user.name} - {user.email} ({user.userRole.description})</div>
+								<button type={'submit'} className={'text-blue-700 underline pl-5'}>delete</button>
 							</div>
 						</Form>
 					))}
 					<br/>
+					<div><strong>User Role to User Map</strong></div>
+					<hr className={'h-[0.5px] border-none text-black bg-black mb-2 p-0'}/>
 					{userRoles.map((role, index) => (
-						<div key={role.id} className={'flex flex-row gap-3'}>
+						<div key={role.id} className={'flex flex-row justify-between'}>
 							<div>{index === 0 ? <strong>Roles</strong> : null}
-							<div>{role.description}</div>
+								<div>{role.description}</div>
 							</div>
 							<div>{index === 0 ? <strong>Users</strong> : null}
-							{role.users.map((user) => (
-								<div key={user.id}>{user.id} - {user.name}</div>
-							))
-							}
+								{role.users.map((user) => (
+									<div key={user.id}>{user.id} - {user.name}</div>
+								))
+								}
 							</div>
 						</div>
 					))}
